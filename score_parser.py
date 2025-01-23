385f6cd7e7c24c208b722a17ebc0dbfe7514afb0
localdmluser <localdmluser@lmb-2203.adms.luc.edu>
2025-01-22 22:33:35 -0600

    neva eva do linear regression up in this piece

--- score_parser.py
+++ score_parser.py
@@ -1,8 +1,12 @@
 import csv
 from collections import defaultdict
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr, spearmanr
+from scipy.optimize import curve_fit
 
-from numpy.ma.extras import average
+def exponential_model(T, beta0, beta1):
+    return beta0 * np.exp(beta1 * T)
 
 with open("royola_ai_essays_DSI.csv", "r") as feil:
     reader = csv.reader(feil)
@@ -17,7 +21,39 @@ store.pop("spec")
 for key, value in store.items():
     store[key] = list(map(float, value))
 
+pts = []
+xval = 0
+for key, value in store.items():
+    for n in value:
+        pts.append((xval, n))
+    xval += 0.2
+
+x, y = zip(*pts)
+print(np.mean(y), np.std(y))
+r = pearsonr(x, y)
+
+params, covariance = curve_fit(exponential_model, x, y, p0=(1, -1))
+print(f"params = {params}")
+print(f"covariance = {covariance}")
+print(params, covariance)
+print("=====")
+print(r[0])
+print(r)
+
+
 with open("sem_dis_scores.txt", "w") as feil:
+    lines = []
     for key, val in store.items():
-        line = f"Temp: {key}; n = {len(val)}; Mean DSI: {np.mean(val):.4f}; Std: {np.std(val):.4f}; Min/Max: ({min(val):.4f}, {max(val):.4f}) \n"
-        feil.write(line)
+        lines.append(f"Temp: {key}; n = {len(val)}; Mean DSI: {np.mean(val):.4f}; Std: {np.std(val):.4f}; Min/Max: ({min(val):.4f}, {max(val):.4f}) \n")
+    with open("sem_dis_scores.txt", "w") as feil:
+        feil.write(f"r = {r[0]:.4f}; p = {r[1]:.4f}\n")
+        for i in lines:
+            feil.write(i)
+
+
+T_fit = np.linspace(min(x), max(y), 100)
+y_fit = exponential_model(T_fit, params[0], params[1])
+
+plt.plot(T_fit, y_fit)
+plt.scatter(x, y)
+plt.show()
